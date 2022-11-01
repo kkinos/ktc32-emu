c4d55b98b0368517741f07853542580e1b6abe1a
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-11-01 23:36:27 +0900

    Rename hex -> test

---
+++ test/fibonacci.asm
@@ -0,0 +1,14 @@
+addi r1, r0, 1
+addi r2, r0, 1
+addi r3, r0, 80
+add r1, r2
+add r2, r1
+add r1, r2
+add r2, r1
+add r1, r2
+add r2, r1
+add r1, r2
+add r2, r1
+add r1, r2
+add r2, r1
+sw r2, r3, 0
